e1517afff3922e9566f14393c64ea0b1c1c4906b
Nick G <54913349+NickG-Math@users.noreply.github.com>
2020-01-04 21:02:14 -0600

    Improved implementation + SFINAE

--- Mackey/Graph.h
+++ Mackey/Graph.h
@@ -3,6 +3,7 @@
 #include<array>
 #include<fstream>
 #include<queue>
+#include "SFINAE.h"
 
 ///@file
 ///@brief Contains weighted/colored graph classes and Dikjstra algorithms.
@@ -14,24 +15,20 @@ namespace Mackey {
 	/// A directed Graph
 
 	/// This is a general class that can only be used by inheriting from it.
-	/// T is the type of the graph eg weightedGraph or coloredGraph. It's used for the CRTP compile-time polymorphism.
+	/// T is the type of the graph eg weightedGraph or coloredGraph. It's used for the CRTP compile-time polymorphism. By default it's void
 	//////////////////////////////////////
-	template<typename T>
+	template<typename T=void>
 	class Graph {
 	public:
 		int number_of_nodes; ///<The number of vertices
 
-		
 		std::vector<std::vector<int>> edges; ///<The edges of the graph
 
-
 		//////////////////////////////////////
-		/// The "shortest" path from a selected source to any other point. 
-
-		/// The meaning of shortest is determined in the specializations of Graph. 
-		
-		///The default by distance where each edge has weight 1.
+		/// The shortest path (in terms of total length of edges) from a selected source to any other point. 
 	
+		/// The length of each edge depends on the specilization of Graph
+
 		///path[i] consists of the procession of nodes from i to source.
 		//////////////////////////////////////
 		std::vector<std::vector<int>> path;
@@ -40,32 +37,28 @@ namespace Mackey {
 		std::vector<std::vector<int>> path_edges;
 		
 		/////////////////////////////////////
-		/// The weighted distance from a selected source to all points. -1 if disconnected
+		/// The distance from a selected source to all points. -1 if disconnected
 
-		///The type of weight will depend on the specialization of Graph.
-		std::vector<int> weightedDistance;
-		
+		/// The length of each edge depends on the specilization of Graph
+		//////////////////////////////////////
+		std::vector<int> distance;
+
+		/// The disconnected points from each source
+		std::vector<int> disconnected;
+
 		/// Construct given the edges of the graph
 		Graph(const std::vector<std::vector<int>>& edges) : number_of_nodes(edges.size()), edges(edges) {}
 
-		/// A general paradigm to compute the paths using the given source and a Dikjstra algorithm. This does not rewrite any previous path computations.
-		void computeWithSource(int givensource) {
-			if (path.size() < number_of_nodes) { //either hasn't been run, or new nodes were added
-				path.resize(number_of_nodes); 
-				weightedDistance.assign(number_of_nodes, -1);
-			}
-			source = givensource;
-			static_cast<T*>(this)->computePath();
-		};
+		/// Compute all paths using the given source
+		void computeWithSource(int givensource);
 
-		/// A general paradigm to compute the paths using the given source and a Dikjstra algorithm, overwritting any previous path computations.
-		void computeWithSource_clear(int givensource) {
-			path.clear();
-			path.resize(number_of_nodes);
-			weightedDistance.assign(number_of_nodes, -1);
-			static_cast<T*>(this)->initialize();
-			computeWithSource(givensource);
-		};
+		///Compute the disconnected nodes up to a certain node
+		void compute_disconnected(int limit);
+
+		///Compute all disconnected nodes
+		void compute_disconnected() {
+			compute_disconnected(number_of_nodes);
+		}
 		
 		/// Writes a graph.dot file representing the graph. The nodes are unnamed.
 		void draw();
@@ -74,16 +67,52 @@ namespace Mackey {
 		///Default constructor
 		Graph() {};
 
-		///General implementation for graphs 
-		void computePath();
+		///Default implementation for graphs uses length=1 for all edges
+		void computePath_default();
+
+		///Clears distances and initializes
+		void clear();
 
 	protected:
 		int source; ///<A source of the graph
-	private:
-		//Default implementation
-		void initialize() {};
-
 	};
+
+
+
+	template<typename T>
+	void Graph<T>::computeWithSource(int givensource){
+		if (path.size() < number_of_nodes) { //either hasn't been run, or new nodes were added
+			path.resize(number_of_nodes);
+			distance.assign(number_of_nodes, -1);
+		}
+		source = givensource;
+		if constexpr (SFINAE::has_computePath<T>::value) //if it has an implementation of computePath
+			static_cast<T*>(this)->computePath();
+		else //otherwise use default implementation with length=1 for all edges
+			computePath_default();
+	}
+
+	template<typename T>
+	void Graph<T>::compute_disconnected(int limit) {
+		disconnected.reserve(limit);
+		disconnected.clear();
+		for (int i = 0; i < limit; i++) {
+			if (distance[i] == -1)
+				disconnected.push_back(i);
+		}
+	}
+
+	template<typename T>
+	void Graph<T>::clear() {
+		path.clear();
+		path.resize(number_of_nodes);
+		distance.assign(number_of_nodes, -1);
+		disconnected.clear();
+		if constexpr (SFINAE::has_initialize<T>::value)
+			static_cast<T*>(this)->initialize(); //initialize
+	}
+
+
 
 	template<typename T>
 	void Graph<T>::draw() {
@@ -125,8 +154,13 @@ namespace Mackey {
 
 		///Constructor from given graph and weights
 		template<typename T>
-		WeightedGraph(Graph<T>& G, const std::vector<std::vector<int>> weights): weights(weights){
-			static_cast<Graph<T>&>(*this) = G;
+		WeightedGraph(const Graph<T>& G, const std::vector<std::vector<int>> weights): weights(weights){
+			number_of_nodes = G.number_of_nodes;
+			edges = G.edges;
+			path = G.path;
+			distance = G.distance;
+			path_edges = G.path_edges;
+			disconnected = G.disconnected;
 			initialize(); 
 		}
 
@@ -137,14 +171,12 @@ namespace Mackey {
 		
 		///Constructor from other graph and zero weights
 		template<typename T>
-		WeightedGraph(Graph<T>& G) : WeightedGraph(G, zeroWeights(G.edges)) { initialize(); }
+		WeightedGraph(Graph<T>& G) : WeightedGraph(G, zeroWeight(G.edges)) { initialize(); }
 		
 	
-	private:
+	public:
 		std::vector<std::vector<int>> weights;
-
-
-		std::vector<int> distance, closest;
+		std::vector<int> unweighted_distance, closest;
 		std::vector<char> visited, reachable; //not bool for performance
 		std::priority_queue<std::pair<int, int>> next;
 		auto zeroWeight(const std::vector<std::vector<int>>&);
@@ -154,6 +186,7 @@ namespace Mackey {
 		void stepPath(int, int);
 		void computeDistance();
 		friend class Graph<WeightedGraph>; ///<Used to set up the CRTP.
+		friend class SFINAE;
 	};
 
 
@@ -169,8 +202,8 @@ namespace Mackey {
 
 	void WeightedGraph::initialize() {
 		visited.assign(number_of_nodes, 0);
-		distance.assign(number_of_nodes, 0);
-		closest = distance;
+		unweighted_distance.assign(number_of_nodes, 0);
+		closest = unweighted_distance;
 	}
 
 
@@ -178,11 +211,11 @@ namespace Mackey {
 		for (size_t j = 0; j < edges[start].size(); j++) {
 			auto i = edges[start][j];
 			if (!visited[i]) {
-				if (weightedDistance[i] == -1 || weightedDistance[i] > weightedDistance[start] + weights[start][j] + 1) {
-					weightedDistance[i] = weightedDistance[start] + weights[start][j] + 1;
-					distance[i] = distance[start] + 1;
+				if (distance[i] == -1 || distance[i] > distance[start] + weights[start][j] + 1) {
+					distance[i] = distance[start] + weights[start][j] + 1;
+					unweighted_distance[i] = unweighted_distance[start] + 1;
 					closest[i] = start;
-					next.push(std::make_pair(-weightedDistance[i], i));
+					next.push(std::make_pair(-distance[i], i));
 				}
 			}
 		}
@@ -193,9 +226,9 @@ namespace Mackey {
 	void WeightedGraph::computeDistance() {
 		reachable.assign(number_of_nodes, 0);
 		closest[source] = source;
-		weightedDistance[source] = 0;
+		distance[source] = 0;
 		int start = source;
-		next.push(std::make_pair(-weightedDistance[source], source));
+		next.push(std::make_pair(-distance[source], source));
 		while (!next.empty()) {
 			start = next.top().second;
 			next.pop();
@@ -214,7 +247,7 @@ namespace Mackey {
 		computeDistance();
 		for (int i = 0; i < number_of_nodes;i++) {
 			if (reachable[i]) {
-				path[i].reserve(distance[i] + 1);
+				path[i].reserve(unweighted_distance[i] + 1);
 				path[i].push_back(i);
 				stepPath(i, i);
 				path[i].push_back(source);
@@ -225,10 +258,12 @@ namespace Mackey {
 	
 
 	template<typename T>
-	void Graph<T>::computePath() {
+	void Graph<T>::computePath_default() {
 		WeightedGraph W(*this);
-		W.computePath();
-		*this=static_cast<Graph<T>&>(W);
+		W.computeWithSource(source);
+		path = std::move(W.path);
+		distance = std::move(W.distance);
+		path_edges = std::move(W.path_edges);
 	}
 
 
@@ -253,13 +288,14 @@ namespace Mackey {
 		/// Writes a graph.dot file representing the colored graph and named nodes and named edges
 		void draw(const std::vector<std::string>&, const std::vector<std::vector<int>>&, std::vector<std::string>&);
 
-	private:
+	public:
 		void computePath();
 		void constructDual();
 		std::vector<int> adjustpath(std::vector<int>& path);
 		WeightedGraph dual;
 
 		friend class Graph<ColoredGraph>; ///<Used to set up the CRTP.
+		friend class SFINAE;
 	};
 
 
@@ -309,35 +345,32 @@ namespace Mackey {
 		constructDual();
 		dual.computeWithSource(2 * source);
 		auto redpath = std::move(dual.path);
-		auto reddistance = std::move(dual.weightedDistance);
-		dual.computeWithSource_clear(2 * source + 1);
+		auto reddistance = std::move(dual.distance);
+		dual.clear();
+		dual.computeWithSource(2 * source + 1);
 		auto bluepath = std::move(dual.path);
-		auto bluedistance = std::move(dual.weightedDistance);
+		auto bluedistance = std::move(dual.distance);
 
 		for (int i = 0; i < number_of_nodes; i++) {
-			std::array<int, 3> V = { reddistance[2 * i + 1], bluedistance[2 * i], bluedistance[2 * i + 1] };
-			int mindistance = reddistance[2 * i];
-			int finder = 0;
-			for (int j = 0; j < 2; j++) {
-				if (V[j] != -1 && mindistance == -1) {
+			int mindistance = -1;
+			std::array<int, 4> V = { reddistance[2 * i], reddistance[2 * i + 1], bluedistance[2 * i], bluedistance[2 * i + 1] };
+			int finder = -1;
+			for (int j = 0; j < 3; j++) {
+				if (V[j] != -1 && (mindistance == -1 || V[j] < mindistance)) {
 					mindistance = V[j];
-					finder = j + 1;
-				}
-				else if (V[j] != -1 && V[j] < mindistance) {
-					mindistance = V[j];
-					finder = j + 1;
+					finder = j;
 				}
 			}
-			if (finder <= 1) {
-				if (weightedDistance[i]==-1 || (weightedDistance[i] > reddistance[2 * i + finder] && reddistance[2 * i + finder]>=0 ) || (weightedDistance[i]==reddistance[2 * i + finder] && path[i].size()>redpath[2 * i + finder].size() && reddistance[2 * i + finder] >= 0)) {
+			if (finder==0 || finder==1) {//use red
+				if (distance[i]==-1 || (distance[i] > reddistance[2 * i + finder] || (distance[i]==reddistance[2 * i + finder] && path[i].size()>redpath[2 * i + finder].size()) )) {
 					path[i] = adjustpath(redpath[2 * i + finder]);
-					weightedDistance[i] = reddistance[2 * i + finder];
+					distance[i] = reddistance[2 * i + finder];
 				}
 			}
-			else {
-				if (weightedDistance[i] == -1 || (weightedDistance[i] > bluedistance[2 * i + finder - 2] && bluedistance[2 * i + finder - 2]>=0) || (weightedDistance[i]==bluedistance[2 * i + finder - 2] && path[i].size()>bluepath[2*i+finder-2].size()&& bluedistance[2 * i + finder - 2] >= 0)) {
+			else if (finder==2 || finder==3){//use blue
+				if (distance[i] == -1 || (distance[i] > bluedistance[2 * i + finder - 2] || (distance[i]==bluedistance[2 * i + finder - 2] && path[i].size()>bluepath[2*i+finder-2].size()))) {
 					path[i] = adjustpath(bluepath[2 * i + finder - 2]);
-					weightedDistance[i] = bluedistance[2 * i + finder - 2];
+					distance[i] = bluedistance[2 * i + finder - 2];
 				}
 			}
 		}

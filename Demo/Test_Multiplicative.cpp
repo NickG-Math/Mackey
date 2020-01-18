412058488bd98124208591d37cf00e4018765ef9
Nick G <54913349+NickG-Math@users.noreply.github.com>
2020-01-17 22:38:59 -0600

    Minor update

--- Demo/Test_Multiplicative.cpp
+++ Demo/Test_Multiplicative.cpp
@@ -4,7 +4,7 @@
 //#define NDEBUG
 
 #ifdef _MSC_VER
-#define _SILENCE_CXX17_ADAPTOR_TYPEDEFS_DEPRECATION_WARNING //Eigen won't work otherwise.
+#define _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS //Eigen won't work otherwise.
 #endif
 //note: These preprocessor directives must be before any includes.
 
@@ -15,11 +15,13 @@
 
 
 typedef Eigen::Matrix<char, 1, -1> rank_t;
-typedef Eigen::Matrix<char, -1, -1> diff_t;
+//typedef Eigen::Matrix<char, -1, -1> diff_t;
+typedef Eigen::SparseMatrix<char,0,long> diff_t;
 
 int main() {
 	auto begin = std::chrono::high_resolution_clock::now();
 	C4Test::C4Multtest<rank_t, diff_t>(13, 13, 13, 13, 0);
 	auto end = std::chrono::high_resolution_clock::now();
 	std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count() << "ns" << std::endl;
+	return 0;
 }

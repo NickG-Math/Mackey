a63c0ff8f89cd47630cf78b402b60691d1a53235
Nick G <54913349+NickG-Math@users.noreply.github.com>
2021-01-31 22:27:55 -0600

    Fixed compatbility with Eigen 3.3.9

--- source/Types/SFINAE.h
+++ source/Types/SFINAE.h
@@ -23,24 +23,35 @@ namespace Mackey {
 	//////////////////
 	struct SFINAE {
 
-
 		//Detect Eigen Dense matrices 
 		static constexpr std::false_type test_Dense(...);
 
 		template<typename T>
-		static constexpr std::true_type test_Dense(Eigen::MatrixBase<T>);
+		static constexpr std::true_type test_Dense(Eigen::MatrixBase<T>*);
 
 		//Detect Eigen Sparse matrices
 		static constexpr	std::false_type test_Sparse(...);
 
 		template <typename T>
-		static constexpr std::true_type test_Sparse(Eigen::SparseMatrixBase<T>);
+		static constexpr std::true_type test_Sparse(Eigen::SparseMatrixBase<T>*);
 
 		//Detect Eigen Sparse Row Major matrices
 		static constexpr	std::false_type test_SRM(...);
 
 		template <typename T>
-		static constexpr std::true_type test_SRM(Eigen::SparseMatrix<T,1>);
+		static constexpr std::true_type test_SRM(Eigen::SparseMatrix<T,1>*);
+
+		///Tests if matrix is dense
+		template<typename T>
+		using is_Dense = decltype(test_Dense(std::declval<T*>()));
+
+		///Tests if matrix is sparse
+		template<typename T>
+		using is_Sparse = decltype(test_Sparse(std::declval<T*>()));
+
+		///Tests if matrix is sparse row major
+		template<typename T>
+		using is_sparse_row_major = decltype(test_SRM(std::declval<T*>()));
 
 
 		//Detect if T=Z<N>
@@ -49,12 +60,22 @@ namespace Mackey {
 		template <int N>
 		static constexpr std::true_type test_finite_cyclic(Z<N>);
 
+		///Tests if T=Z<N>
+		template<typename T>
+		using is_finite_cyclic = decltype(test_finite_cyclic(std::declval<T>()));
+
+
 		//Detect implementation of computePath()
 		template<typename T>
 		static constexpr std::false_type test_computePath(...);
 
 		template<typename T>
 		static constexpr decltype(std::declval<T>().computePath(), std::true_type()) test_computePath(int);
+
+		///Tests if T has member computePath()
+		template<typename T>
+		using has_computePath = decltype(test_computePath<T>(0));
+
 
 		//Detect implementation of initialize()
 
@@ -63,28 +84,6 @@ namespace Mackey {
 
 		template<typename T>
 		static constexpr decltype(std::declval<T>().initialize(), std::true_type()) test_initialize(int);
-
-		
-		///Tests if matrix is dense
-		template<typename T>
-		using is_Dense = decltype(test_Dense(std::declval<T>()));
-
-		///Tests if matrix is sparse
-		template<typename T>
-		using is_Sparse = decltype(test_Sparse(std::declval<T>()));
-
-		///Tests if T=Z<N>
-		template<typename T>
-		using is_finite_cyclic = decltype(test_finite_cyclic(std::declval<T>()));
-
-		///Tests if matrix is sparse row major
-		template<typename T>
-		using is_sparse_row_major = decltype(test_SRM(std::declval<T>()));
-
-
-		///Tests if T has member computePath()
-		template<typename T>
-		using has_computePath = decltype(test_computePath<T>(0));
 
 		///Tests if T has member initialize()
 		template<typename T>

38fdf39e524c31c2f6421bac4fa6121684183ee9
Nick G <54913349+NickG-Math@users.noreply.github.com>
2021-04-25 17:42:36 -0500

    Added generators for combinations etc.

---
+++ source/Utility/Generators.hpp
@@ -0,0 +1,133 @@
+#pragma once
+#include <algorithm>
+#include <vector>
+#include <array>
+
+///	@file
+///	@brief Contains classes for generating permutations, combinations and a factory for such classes
+
+namespace mackey
+{
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////
+	///	@brief			Prototype for coroutine-like iterators that generate elements such as combinations.
+	///	@details		Inherit from this class and define a method update() to get a const iterator. \n
+	///					You will also need \c begin() and \c end() methods returning such iterators;
+	///					\c end() should always be defined by calling the factory \c end() .\n
+	///					Example implementations: \c CombinationGenerator and \c PermutationGenerator
+	///	@attention		Probably not thread-safe (depends on specialization's \c update() method).
+	///	@todo			Implement as coroutine (C++20)
+	///	@tparam	spec_t	Used for compile-time polymorphism (CRTP): set it to be the child class.
+	///	@tparam	gen_t	The type of the generated element.
+	////////////////////////////////////////////////////////////////////////////////////////////////////////
+	template <class spec_t, class gen_t>
+	class FactoryGenerator
+	{
+	public:
+		const gen_t &operator*() const;						  ///<Returns the generated element
+		bool operator!=(const FactoryGenerator &other) const; ///<Inequality of iterators (used to detect if generation has been completed)
+		spec_t &operator++();								  ///<Generates next element
+		static spec_t end();								  ///<Terminal iterator
+	protected:
+		bool completed;	 ///<1 if the iterator is end() i.e. if all elements have been generated
+		gen_t generated; ///<The currently generated element
+	};
+
+	////////////////////////////////////////////////////////////////////////////////////////
+	///	@brief		Generates all permutations on a number of letters
+	///	@details	Use with a ranged for loop: If \c v is a \c PermutationGenerator object then in
+	///				\code for (const auto& i:v) {...} \endcode the variable \c i will range over all permutations.
+	///	@warning	Not thread safe!
+	///	@todo		Implement via coroutine (C++20)
+	///	@tparam T	The data type of our permutations eg \c int
+	//////////////////////////////////////////////////////////////////////////////////
+	template <class T>
+	class PermutationGenerator
+	{
+		const T n;
+
+	public:
+		/// @brief	Computes total number of permutations
+		/// @return Factorial \f$n!\f$ where \f$n\f$ is the number of letters
+		size_t size() const;
+
+		///	@brief		Constructor sets up the generator
+		///	@param n	The total number of letters
+		PermutationGenerator(T n);
+
+		///	@brief		Constant iterator that is used in a ranged for loop to generate the permutations.
+		///	@warning	Non constant version is illegal
+		class ConstIterator : public FactoryGenerator<PermutationGenerator::ConstIterator, std::vector<T>>
+		{
+			void update();
+			friend class PermutationGenerator;													///<Befriending outer class
+			friend class FactoryGenerator<PermutationGenerator::ConstIterator, std::vector<T>>; ///<Befriending parent
+		};
+
+		/// @brief	Begin iterator
+		/// @return An iterator to the first generated element
+		ConstIterator begin() const;
+
+		/// @brief	End iterator
+		/// @return An iterator to the end of the generator (equality with this indicates that the generator has completed)
+		ConstIterator end() const;
+	};
+
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	///	@brief		Generates all combinations on a number of letters making a number of choices
+	///	@details	Use with a ranged for loop: If \c v is a \c CombinationGenerator object then in
+	///				\code for (const auto& i:v) {...} \endcode the variable \c i will range over all combinations.
+	///	@warning	Not thread safe!
+	///	@todo		Implement via coroutine (C++20)
+	///	@tparam  T	The data type of our combinations eg \c int
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	template <class T>
+	class CombinationGenerator
+	{
+		const T total, choices;
+
+	public:
+		///	@brief	Computes total number of combinations
+		///	@return Binomial \f${n}\choose{k}\f$ where \f$n\f$=total and \f$k\f$=choices
+		auto size() const;
+
+		///	@brief			Sets up the generator
+		///	@param total	The number of letters
+		///	@param choices	The number of choices
+		CombinationGenerator(T total, T choices);
+
+		///	@brief		Constant iterator that is used in a ranged for loop to generate the combinations.
+		///	@warning	Non \c const version is illegal
+		class ConstIterator : public FactoryGenerator<CombinationGenerator::ConstIterator, std::vector<T>>
+		{
+			void update();
+			friend class CombinationGenerator;													///<Befriending outer class
+			friend class FactoryGenerator<CombinationGenerator::ConstIterator, std::vector<T>>; ///<Befriending parent
+			T total, choices;
+		};
+
+		/// @brief	Begin iterator
+		/// @return An iterator to the first generated element
+		ConstIterator begin() const;
+
+		/// @brief	End iterator
+		/// @return An iterator to the end of the generator (equality with this indicates that the generator has completed)
+		ConstIterator end() const;
+	};
+
+	///	@brief		Returns vector of all permutations on \p n letters
+	///	@tparam	T	The value type of the permutations
+	///	@param	n	The number of letters
+	///	@return		Vector of vectors each of which is \f$[a_0,...,a_{n-1}]\f$ with \f$0\le a_i\le n-1\f$ all distinct
+	template <class T>
+	std::vector<std::vector<T>> all_permutations(T n);
+
+	///	@brief		Returns vector of all combinations on \p n letters choosing \p m many
+	///	@tparam	T	The value type of the combinations
+	///	@param	n	The number of letters
+	///	@param	m	The number of choices
+	///	@return		Vector of vectors each of which is \f$[a_0,...,a_{n-1}]\f$ with \f$0\le a_i\le n-1\f$ all distinct
+	template <class T>
+	std::vector<std::vector<T>> all_combinations(T n, T m);
+}
+#include "impl/Generators.ipp"
---
+++ source/Utility/impl/Generators.ipp
@@ -0,0 +1,157 @@
+#pragma once
+#include "../Generators.hpp"
+
+///@file
+///@brief Implementation of Generators.hpp
+
+namespace mackey
+{
+
+	template <typename spec_t, typename gen_t>
+	const gen_t &FactoryGenerator<spec_t, gen_t>::operator*() const
+	{
+		return generated;
+	}
+
+	template <typename spec_t, typename gen_t>
+	bool FactoryGenerator<spec_t, gen_t>::operator!=(const FactoryGenerator &other) const
+	{
+		if (other.completed) //quick check if other=end
+			return !completed;
+		else
+			return generated == other.generated;
+	}
+
+	template <typename spec_t, typename gen_t>
+	spec_t &FactoryGenerator<spec_t, gen_t>::operator++()
+	{
+		static_cast<spec_t *>(this)->update();
+		return *static_cast<spec_t *>(this);
+	}
+
+	template <typename spec_t, typename gen_t>
+	spec_t FactoryGenerator<spec_t, gen_t>::end()
+	{
+		spec_t end;
+		end.completed = 1;
+		return end;
+	}
+
+	template <typename T>
+	PermutationGenerator<T>::PermutationGenerator(T n) : n(n) {}
+
+	template <typename T>
+	size_t PermutationGenerator<T>::size() const
+	{
+		size_t factorial = 1;
+		for (int i = 2; i <= n; i++)
+		{
+			factorial *= i;
+		}
+		return factorial;
+	}
+
+	template <typename T>
+	void PermutationGenerator<T>::ConstIterator::update()
+	{
+		if (!std::next_permutation(this->generated.begin(), this->generated.end()))
+			this->completed = 1;
+	}
+
+	template <typename T>
+	typename PermutationGenerator<T>::ConstIterator PermutationGenerator<T>::begin() const
+	{
+		ConstIterator it;
+		it.generated.resize(n);
+		std::iota(it.generated.begin(), it.generated.end(), 0);
+		it.completed = 0;
+		return it;
+	}
+
+	template <typename T>
+	typename PermutationGenerator<T>::ConstIterator PermutationGenerator<T>::end() const
+	{
+		return FactoryGenerator<PermutationGenerator::ConstIterator, std::vector<T>>::end();
+	}
+
+	template <typename T>
+	CombinationGenerator<T>::CombinationGenerator(T total, T choices) : total(total), choices(choices)
+	{
+		if (choices > total)
+		{
+			std::cerr << "You can't choose more elements than those existing!";
+			abort();
+		}
+	}
+
+	template <typename T>
+	auto CombinationGenerator<T>::size() const
+	{
+		//safe implementation
+		auto lowchoices = (choices > total - choices) ? total - choices : choices;
+		auto binom = total;
+		for (int i = 1; i <= lowchoices - 1; i++)
+		{ // n/1 * (n-1)/2 * \cdots (n-k+1)/k
+			binom *= (total - i);
+			binom /= i + 1;
+		}
+		return binom;
+	}
+
+	template <typename T>
+	void CombinationGenerator<T>::ConstIterator::update()
+	{
+		for (int64_t i = (int64_t)choices - 1; i >= 0; i--)
+		{ //must be signed!
+			if (this->generated[i] < total - choices + i)
+			{
+				this->generated[i]++;
+				for (auto j = i + 1; j < (int64_t)choices; j++)
+					this->generated[j] = this->generated[j - 1] + 1;
+				return;
+			}
+		}
+		this->completed = 1;
+		return;
+	}
+
+	template <typename T>
+	typename CombinationGenerator<T>::ConstIterator CombinationGenerator<T>::begin() const
+	{
+		ConstIterator it;
+		it.generated.resize(choices);
+		std::iota(it.generated.begin(), it.generated.end(), 0);
+		it.total = total;
+		it.choices = choices;
+		it.completed = 0;
+		return it;
+	}
+
+	template <typename T>
+	typename CombinationGenerator<T>::ConstIterator CombinationGenerator<T>::end() const
+	{
+		return FactoryGenerator<CombinationGenerator::ConstIterator, std::vector<T>>::end();
+	}
+
+	template <typename T>
+	std::vector<std::vector<T>> all_permutations(T n)
+	{
+		std::vector<std::vector<T>> v;
+		PermutationGenerator<T> p(n);
+		v.reserve(p.size());
+		for (const auto &i : p)
+			v.push_back(i);
+		return v;
+	}
+
+	template <typename T>
+	std::vector<std::vector<T>> all_combinations(T n, T m)
+	{
+		std::vector<std::vector<T>> v;
+		CombinationGenerator<T> p(n, m);
+		v.reserve(p.size());
+		for (const auto &i : p)
+			v.push_back(i);
+		return v;
+	}
+}
